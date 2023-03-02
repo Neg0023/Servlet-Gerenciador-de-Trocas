0937cbbe8a78adb4ba8e7715fd2a179fab1b2a9d
Uemerson Santos <uemerson161@hotmail.com>
2023-03-01 21:25:48 -0300

    classe model produto criada

---
+++ src/main/java/br/com/dominio/gerenciadorDeTrocas/model/Produto.java
@@ -0,0 +1,42 @@
+package br.com.dominio.gerenciadorDeTrocas.model;
+
+public class Produto {
+
+	private Long id;
+	private String sku;
+	private String nomeProduto;
+	private Integer quantidade;
+	private Double preco;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	public String getSku() {
+		return sku;
+	}
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	
+	
+}
